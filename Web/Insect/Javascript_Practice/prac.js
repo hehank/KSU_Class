c489c12ade7266a1c787ea46fc48ce752aa79948
hehank <46835548+hehank@users.noreply.github.com>
2021-12-04 21:28:25 +0800

    2021/12/04 JS study

--- Web/Insect/Javascript_Practice/prac.js
+++ Web/Insect/Javascript_Practice/prac.js
@@ -1,42 +1,50 @@
-function factorial_loop(num)
-{
-	var sum = 1;
-	
-	if (num == 0)
-	{
-		sum = 0;
-		return sum;
-	}
-	else if (num == 1)
-	{
-		return sum;
-	}
-	else
-	{
-		for (let i = 1; i <= num; i++)
-		{
-			sum *= i;
-		}
-	}
-	
-	return sum;
+function factorial_loop(num) {
+    var sum = 1;
+
+    if (num == 0) {
+        sum = 0;
+        return sum;
+    } else if (num == 1) {
+        return sum;
+    } else if (num < 0) {
+        return "Can't understand English?";
+    } else if (num % 1 != 0) {
+        return "Can't understand English?";
+    } else {
+        for (let i = 1; i <= num; i++) {
+            sum *= i;
+        }
+
+        return sum;
+    }
 }
 
-function factorial_recursive(num, sum)
-{
-	if (num == 0)
-	{
-		return sum;
-	}
-	
-	return factorial_recursive(num - 1, sum * num);
+function loop_work() {
+    var loop_inp_value = document.getElementById("loop").value;
+
+    var loop_fac = factorial_loop(loop_inp_value);
+
+    document.getElementById("loop_fac_ans").innerText =
+        "Answer：" + loop_fac.toString();
 }
 
-var loop_inp_value = document.getElementById("loop").value;
-var recu_inp_value = document.getElementById("recursive").value;
+function factorial_recursive(num, sum) {
+    if (num == 0) {
+        return sum;
+    } else if (num < 0) {
+        return "Can't understand English?";
+    } else if (num % 1 != 0) {
+        return "Can't understand English?";
+    }
 
-var loop_fac = factorial_loop(loop_inp_value);
-var recu_fac = factorial_recursive(recu_inp_value, recu_inp_value);
+    return factorial_recursive(num - 1, sum * num);
+}
 
-document.getElementById("loop_fac_ans").innerHTML = "Answer：" + loop_fac.toString();
-document.getElementById("recu_fac_ans").innerHTML = "Answer：" + recu_fac.toString();
+function recu_work() {
+    var recu_inp_value = document.getElementById("recursive").value;
+
+    var recu_fac = factorial_recursive(recu_inp_value, 1);
+
+    document.getElementById("recu_fac_ans").innerText =
+        "Answer：" + recu_fac.toString();
+}
