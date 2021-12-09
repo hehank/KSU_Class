84775358b7bc79dff2fe6babe4e239eb021fc8cb
hehank <46835548+hehank@users.noreply.github.com>
2021-12-09 10:21:49 +0800

    2021/12/08 study

--- Web/Insect/Javascript_Practice/test.js
+++ Web/Insect/Javascript_Practice/test.js
@@ -1,9 +1,2 @@
-var global_var = "This is global.";
-
-function newFunction() {
-    var local_var = "This is local.";
-    console.log(local_var);
-}
-
-console.log(global_var);
-console.log(local_var); // local_var is not defined
+const n = 10;
+n = 5;
