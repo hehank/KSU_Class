098a0fd377304956ed4c7341186c80de51403f99
hehank <46835548+hehank@users.noreply.github.com>
2022-01-12 05:49:00 +0800

    2022/01/12 study finish

--- Web/Insect/Javascript_Practice/script1.js
+++ Web/Insect/Javascript_Practice/script1.js
@@ -19,19 +19,6 @@ function factorial_loop(num) {
     }
 }
 
-function factorial_recursive(num, sum) {
-    if (num == 0) {
-        sum = 0;
-        return sum;
-    } else if (num < 0) {
-        return "Can't understand English?";
-    } else if (num % 1 != 0) {
-        return "Can't understand English?";
-    }
-
-    return factorial_recursive(num - 1, sum * num);
-}
-
 function loop_work() {
     let loop_inp_value = document.getElementById("loop").value;
 
@@ -39,13 +26,4 @@ function loop_work() {
 
     document.getElementById("loop_fac_ans").innerText =
         "Answer：" + loop_fac.toString();
-}
-
-function recu_work() {
-    let recu_inp_value = document.getElementById("recursive").value;
-
-    let recu_fac = factorial_recursive(recu_inp_value, 1);
-
-    document.getElementById("recu_fac_ans").innerText =
-        "Answer：" + recu_fac.toString();
 }
