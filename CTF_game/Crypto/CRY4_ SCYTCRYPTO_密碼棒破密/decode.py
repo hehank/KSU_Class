3e0a0e4b86dfedf9d80b2ca95f0cebd429dcffa8
hehank <macb3634919@gmail.com>
2023-03-02 21:53:42 +0800

    update

---
+++ CTF_game/Crypto/CRY4_ SCYTCRYPTO_密碼棒破密/decode.py
@@ -0,0 +1,27 @@
+def scytale_decrypt(ciphertext, key):
+    # 計算每個帶子的字符數
+    num_cols = len(ciphertext) // key
+    if len(ciphertext) % key != 0:
+        num_cols += 1
+
+    # 將密文分成 num_cols 列
+    matrix = [''] * num_cols
+    col = 0
+    for c in ciphertext:
+        matrix[col] += c
+        col = (col + 1) % num_cols
+
+    # 按列讀取字符
+    plaintext = ''
+    for row in range(num_cols):
+        plaintext += matrix[row]
+
+    return plaintext
+
+
+if __name__ == '__main__':
+    ciphertext = 'SDNOLEAEERTRCROACNDFMTNIEOOAE__'
+    key = 5
+
+    plaintext = scytale_decrypt(ciphertext, key)
+    print(plaintext)
---
+++ CTF_game/Crypto/salad-100/caesar.py
@@ -0,0 +1,95 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+up_alpha = [chr(x) for x in range(65, 91)]
+low_alpha = [chr(x) for x in range(97, 123)]
+
+for i in number:
+    up_alpha.append(i)
+    low_alpha.append(i)
+
+
+def decode(cipher, key):
+    plain = ""
+    length = -1 * len(up_alpha)
+
+    for i in cipher:
+        index = 0
+        char = ""
+        deter = False
+
+        if ((i not in up_alpha) and (i not in low_alpha)):
+            plain += i
+            continue
+
+        for j in up_alpha:
+            if (i == j):
+                index = up_alpha.index(j)
+                char = up_alpha[(index - key) % length]
+                plain += char
+                deter = True
+                break
+
+        if deter:
+            continue
+
+        # for k in low_alpha:
+        #     if (i == k):
+        #         index = low_alpha.index(k)
+        #         char = low_alpha[(index + key) % length]
+        #         break
+
+        for k in low_alpha:
+            if (i == k):
+                index = low_alpha.index(k)
+                char = up_alpha[(index - key) % length]
+                plain += char
+                break
+
+    return plain
+
+
+def encode(cipher, key):
+    plain = ""
+    length = len(up_alpha)
+
+    for i in cipher:
+        index = 0
+        char = ""
+        deter = False
+
+        if ((i not in up_alpha) and (i not in low_alpha)):
+            plain += i
+            continue
+
+        for j in up_alpha:
+            if (i == j):
+                index = up_alpha.index(j)
+                char = up_alpha[(index + key) % length]
+                plain += char
+                deter = True
+                break
+
+        if deter:
+            continue
+
+        for k in low_alpha:
+            if (i == k):
+                index = low_alpha.index(k)
+                char = low_alpha[(index + key) % length]
+                break
+
+        # for k in low_alpha:
+        #     if (i == k):
+        #         index = low_alpha.index(k)
+        #         char = up_alpha[(index + key) % length]
+        #         plain += char
+        #         break
+
+    return plain
+
+
+cipher = '7sj-ighm-742q3w4t'
+key = 20
+print(encode(cipher, key))
