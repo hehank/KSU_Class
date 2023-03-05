e80e79ae56b8d6c916ed245524b4af77fbff8428
hehank <macb3634919@gmail.com>
2023-03-05 22:31:10 +0800

    update

--- CTF_game/Python/IF幸福企業的獎金制度/code.py
+++ CTF_game/Python/IF幸福企業的獎金制度/code.py
@@ -6,13 +6,13 @@ bonus = 0
 
 if annual_performance <= 1000000:
     bonus = round(annual_performance * 0.1, 0)
-elif annual_performance <= 2000000:
+elif annual_performance < 2000000:
     bonus = round((annual_performance - 1000000) * 0.07, 0)
-elif annual_performance <= 4000000:
+elif annual_performance < 4000000:
     bonus = round((annual_performance - 2000000) * 0.05, 0)
-elif annual_performance <= 6000000:
+elif annual_performance < 6000000:
     bonus = round((annual_performance - 4000000) * 0.04, 0)
-elif annual_performance <= 10000000:
+elif annual_performance < 10000000:
     bonus = round((annual_performance - 6000000) * 0.02, 0)
 else:
     bonus = round((annual_performance - 10000000) * 0.01, 0)
